4ad6b1c5b304a76df4a34500c91e1b474a5f934f
Kishin Kato <kishin@trustless-services.com>
2022-02-19 01:18:24 +0900

    Renamed db folder

--- db/index.js
+++ db/index.js
@@ -1,31 +1,34 @@
 const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 ;(async () => {
   const client = await pool.connect()
   await client.query({
     text: `
-      CREATE TABLE IF NOT EXISTS "users" (
+      CREATE TABLE IF NOT EXISTS public."Users"(
         "username" VARCHAR(100) UNIQUE NOT NULL,
         "credId" VARCHAR(1000),
         "pubKeyBytes" VARCHAR(256),
         PRIMARY KEY ("username")
-      )
+      );
     `,
     values: []
   })
   await client.query({
     text: `
-    CREATE TABLE IF NOT EXISTS "challenges" (
+    CREATE TABLE IF NOT EXISTS public."Challenges"(
       "username" VARCHAR(100) NOT NULL,
       "challenge" VARCHAR(100) NOT NULL,
       "exipration" VARCHAR(30) NOT NULL,
-      PRIMARY KEY ("challenge"),
-      FOREIGN KEY ("username") REFERENCES "users" [ "username" ]
-    )`
+      PRIMARY KEY ("challenge")
+    );`
   })
   client.release()
 })
