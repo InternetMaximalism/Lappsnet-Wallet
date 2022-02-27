d5fa37c3102a2de8cfd181307271801c88a4b31e
Kishin Kato <kishin@trustless-services.com>
2022-02-27 23:15:09 +0900

    Now showing ESAT balance

--- public/js/web3custom.js
+++ public/js/web3custom.js
@@ -3,6 +3,14 @@ let web3js;
 function initWeb3 () {
   console.log('Loading web3js')
   web3js = new Web3('https://rpc.intmedium.xyz')
+  // After web3js initialized, get user ESAT balance
+  web3js.eth.getBalance(
+      userAddress,
+      "latest",
+      function(err, res)  {
+          if (err) return console.error(err)
+          $('#esatBalance').text(web3js.utils.fromWei(res))
+      })
 }
 
 window.addEventListener('load', initWeb3)
