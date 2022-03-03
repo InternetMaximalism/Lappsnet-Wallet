1e089e1c399e38fd6907566eef99459948338f79
Kishin Kato <kishin@trustless-services.com>
2022-03-03 16:09:35 +0900

    getTokenBalances and getAbi implemented

--- public/js/utils/web3custom.js
+++ public/js/utils/web3custom.js
@@ -14,3 +14,49 @@ function initWeb3 () {
 }
 
 window.addEventListener('load', initWeb3)
+
+async function getTokenBalances (address) {
+    try {
+        let tokenList = (await $.get('https://explorer.intmedium.xyz/api?module=account&action=tokenlist&address='.concat(address))).result
+        if (tokenList.length === 0) {
+            return null
+        }
+        // Otherwise, add balances to balances list & token send selection
+        $('#tokenCount').text(tokenList.length)
+        let balancesDiv = $('#collapseTokenList')
+        balancesDiv.empty()
+        let ul = $('<ul>').appendTo(balancesDiv)
+        tokenList.forEach(i => {
+            let li = $('<li>')
+                      .text(`${escapeHTML(i.name)} (${escapeHTML(i.symbol)} - ${escapeHTML(web3js.utils.fromWei(i.balance))})`)
+                      .attr('class', `tokenListItem m-1`)
+                      .attr('id', `${escapeHTML(i.symbol)}`)
+                      .appendTo(ul)
+        })
+        $('#tokenBalances').show()
+        return tokenList
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function queryTokenList (tokenList, symbol) {
+    console.log(tokenList)
+    return (tokenList.filter(i => {
+        return i.symbol === symbol
+    }))[0]
+}
+
+/* Return ABI for given verified contract */
+async function getAbi (contractAddress) {
+    try {
+        let abi = await ($.get('https://explorer.intmedium.xyz/api?module=contract&action=getabi&address='.concat(contractAddress))).result
+        if (abi === null) {
+            return null
+        }
+        // Otherwise, return abi as object
+        return JSON.parse(abi)
+    } catch (err) {
+        console.error(err)
+    }
+}
