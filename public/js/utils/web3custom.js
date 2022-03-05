54c49b15204acb35455b2e7c9197d7a82247ee20
Kishin Kato <katokishi@gmail.com>
2022-03-06 01:11:47 +0900

    getBalance() implemented

--- public/js/utils/web3custom.js
+++ public/js/utils/web3custom.js
@@ -4,16 +4,26 @@ function initWeb3 () {
   console.log('Loading web3js')
   web3js = new Web3('https://rpc.intmedium.xyz')
   // After web3js initialized, get user ESAT balance
-  web3js.eth.getBalance(
-      userAddress,
-      "latest",
-      function(err, res)  {
-          if (err) return console.error(err)
-          $('#esatBalance').text(web3js.utils.fromWei(res))
-      })
+  if (userAddress) {
+      getBalance(userAddress)
+  }
 }
 
 window.addEventListener('load', initWeb3)
+
+async function getBalance (address) {
+    try {
+        web3js.eth.getBalance(
+            userAddress,
+            "latest",
+            function(err, res)  {
+                if (err) return console.error(err)
+                $('#esatBalance').text(web3js.utils.fromWei(res))
+            })
+    } catch (err) {
+        console.error(err)
+    }
+}
 
 async function getTokenBalances (address) {
     try {
